1822d41eb4b3ae94f7926e83a249eb1d5a3dab92
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-21 21:51:32 -0600

    display events

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+const DUMMY_EVENT = [
+  {
+    id: "e1",
+    title: "A dummy event",
+    date: "2023-02-22",
+    image:
+      "https://blog.hubspot.de/hubfs/Germany/Blog_images/Optimize_Marketing%20Events%20DACH%202021.jpg",
+    description: "Join this amazing event and connect with fellow developers.",
+  },
+];
 const EventsPage = () => {
+    console.log(DUMMY_EVENT)
   return (
-    <div>EventsPage</div>
-  )
-}
+    <>
+      <ul>
+        {DUMMY_EVENT.map((event) => (
+          <li key={event.id}>{event.title}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
 
-export default EventsPage
+export default EventsPage;
