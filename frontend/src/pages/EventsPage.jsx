1f4b81ef05ee661e99d3ec32842a8e737b44873e
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-22 22:43:30 -0600

    clean up events page

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,28 +1,10 @@
-import { useEffect, useState } from 'react';
-
-import EventsList from '../components/EventsList';
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [fetchedEvents, setFetchedEvents] = useState();
-  const [error, setError] = useState();
 
-  useEffect(() => {
-    async function fetchEvents() {
-      setIsLoading(true);
-      
-      setIsLoading(false);
-    }
-
-    fetchEvents();
-  }, []);
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
-        {isLoading && <p>Loading...</p>}
-        {error && <p>{error}</p>}
-      </div>
-      {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
+      <EventsList events={fetchedEvents} />
     </>
   );
 }
