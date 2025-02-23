c0ed444976ddde3163d1baf7986bce73929394a7
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-22 22:44:52 -0600

    useLoaderData

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,10 +1,12 @@
+import { useLoaderData } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-
+const events = useLoaderData()
+console.log(events)
   return (
     <>
-      <EventsList events={fetchedEvents} />
+      <EventsList events={events} />
     </>
   );
 }
